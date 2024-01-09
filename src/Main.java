11c7774edb13464f48ed4543f9aed353f028a0b2
zhyie <128297362+zhyie@users.noreply.github.com>
2024-01-09 15:11:35 +0800

    refresh branch

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main extends ChatBot {
-    public static void main(String[] args) throws InterruptedException {
-        ChatBot chatbot = new ChatBot();
-        chatbot.readInput();
-        chatbot.writeOutput();
-    }
-}
