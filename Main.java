7d44846c90db8ec9a6834aad5591be6652386dee
zhyie <128297362+zhyie@users.noreply.github.com>
2024-01-09 15:20:35 +0800

    update files

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main extends ChatBot{
+        public static void main(String[] args) throws InterruptedException {
+            ChatBot chatbot = new ChatBot();
+            chatbot.readInput();
+            chatbot.writeOutput();
+        }
+}
